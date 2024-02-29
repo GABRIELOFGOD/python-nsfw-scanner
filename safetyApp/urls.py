751bbdfe17ea154563c1981d65693c8f47d49863
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2024-02-29 14:20:13 +0100

    about to deploy

--- safetyApp/urls.py
+++ safetyApp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home' ),
-    path('post', views.posts, name='post')
+    path('', views.index, name='home' )
 ]
